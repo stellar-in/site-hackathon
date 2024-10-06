20dcfb4b693b4dd7768ef40270539e9f5f239ece
Bekhruz Kurbonov <bekhruz@Bekhruzs-MacBook-Air.local>
2024-10-06 20:58:54 +0500

    Main commit

--- main-site.py
+++ main-site.py
@@ -8,5 +8,47 @@ app.config.from_object(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/types")
+def types():
+    return render_template("exoplanets.html")
+
+@app.route("/types/gas-giants")
+def gasGiants():
+    return render_template("gas-giants.html")
+
+@app.route("/51-Pegasi")
+def pegasi_51():
+    return render_template("51-pegasi.html")
+
+@app.route("/types/neptunian-planets")
+def neptunian():
+    return render_template("neptunian-planets.html")
+
+@app.route("/hat-p")
+def hat_p():
+    return render_template("hat-p.html")
+
+@app.route("/types/super-earth")
+def super_earth():
+    return render_template("super-earth.html")
+
+@app.route("/toi-700")
+def toi_700():
+    return render_template("toi-700.html")
+
+@app.route("/toi-system")
+def toi_system():
+    return render_template("toi-700-system.html")
+
+@app.route("/types/terristrial-planets")
+def terristrial():
+    return render_template("terristrial-planets.html")
+
+@app.route("/kepler-78")
+def kepler_78():
+    return render_template("kepler-78.html")
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
